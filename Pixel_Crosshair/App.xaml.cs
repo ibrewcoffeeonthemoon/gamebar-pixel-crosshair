5c89bb867aa57b7d5bb6a00073e8e1d3941c958a
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-12-18 19:56:55 +0800

    rename namespace

--- Pixel_Crosshair/App.xaml.cs
+++ Pixel_Crosshair/App.xaml.cs
@@ -16,7 +16,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Gaming.XboxGameBar;
 
-namespace WidgetSampleCS
+namespace Pixel_Crosshair
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- Pixel_Crosshair/Properties/AssemblyInfo.cs
+++ Pixel_Crosshair/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WidgetSampleCS")]
+[assembly: AssemblyTitle("Pixel_Crosshair")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WidgetSampleCS")]
+[assembly: AssemblyProduct("Pixel_Crosshair")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
