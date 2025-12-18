d234f1bcfefef933dd2632b6b5444226800c3a37
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-12-19 02:34:11 +0800

    impl OnColorPickerColorChanged()

--- Pixel_Crosshair/WidgetPage.xaml.cs
+++ Pixel_Crosshair/WidgetPage.xaml.cs
@@ -3,7 +3,9 @@ using Microsoft.Gaming.XboxGameBar;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,8 +26,8 @@ namespace Pixel_Crosshair
         {
             widget = e.Parameter as XboxGameBarWidget;
 
-			widget.GameBarDisplayModeChanged += OnGameBarDisplayModeChanged;
-		}
+            widget.GameBarDisplayModeChanged += OnGameBarDisplayModeChanged;
+        }
 
         private void OnGameBarDisplayModeChanged(XboxGameBarWidget sender, object args)
         {
@@ -39,8 +41,21 @@ namespace Pixel_Crosshair
         }
 
         private async void OnCenterAppButtonClick(object sender, RoutedEventArgs e)
-		{
-			await widget.CenterWindowAsync();
+        {
+            await widget.CenterWindowAsync();
+        }
+
+        private async void OnColorPickerColorChanged(object sender, ColorChangedEventArgs e)
+        {
+			// Use a SolidColorBrush with the new color
+			SolidColorBrush newBrush = new SolidColorBrush(e.NewColor);
+			// Update all Rectangles inside the CrosshairContainer
+			foreach (var child in CrosshairPreviewGrid.Children)
+			{
+				if (child is Rectangle rect)
+					rect.Fill = newBrush;
+			}
+
 		}
-	}
+    }
 }
