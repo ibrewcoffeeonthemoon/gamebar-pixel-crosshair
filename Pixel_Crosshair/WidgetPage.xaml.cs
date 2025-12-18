75312f321dd85d8444412e71ba70ceb16dafea91
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-12-19 00:40:54 +0800

    button to center app

--- Pixel_Crosshair/WidgetPage.xaml.cs
+++ Pixel_Crosshair/WidgetPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Gaming.XboxGameBar;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -10,13 +14,21 @@ namespace Pixel_Crosshair
     /// </summary>
     public sealed partial class WidgetPage : Page
     {
+        private XboxGameBarWidget widget = null;
+
         public WidgetPage()
         {
             this.InitializeComponent();
         }
-        private void MyButton_Click(object sender, RoutedEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            myButton.Content = "Clicked";
+            widget = e.Parameter as XboxGameBarWidget;
         }
-    }
+
+		private async void OnCenterAppButtonClick(object sender, RoutedEventArgs e)
+		{
+			await widget.CenterWindowAsync();
+		}
+
+	}
 }
